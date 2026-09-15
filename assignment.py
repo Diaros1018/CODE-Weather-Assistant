33049c842fac5e2a903c1144826fae0b93a9200f
Michigan <dlyanez@miners.utep.edu>
2026-09-15 11:33:45 -0600

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,4 +1,4 @@
-# Name
+# Name: Diana Yanez
 # CIS 3330
 # CODE 1 - Weather Assistant
 # Conversion formula: (Temperature in °F - 32) * .5556
@@ -6,7 +6,16 @@
 # "What is the temperature outside: "
 
 def main():
-    pass  # replace this line with your code
+    temperature_f = float(input("What is the temperature outside: "))
+
+    temperature_c = (temperature_f - 32) * .5556
+
+    if temperature_c > 20:
+        print("\nWear a hat")
+    elif temperature_c >= 10:
+        print("\nWear a light jacket")
+    else:
+        print("\nWear a heavy jacket")
 
 
 if __name__ == "__main__":
